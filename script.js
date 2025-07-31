b86b5ac5f901b17189cf6cdbfb5575caa365a22e
selemani <jumaamshakamai@gmail.com>
2025-07-31 22:07:34 +0300

    feat: Add initial JavaScript file and practice variables

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello from script.js!");
+
+const name = "suleimani";
+const age = 21;
+const isStudent= true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is a Student?", isStudent);
+
+let info = `My name is ${name} and I am ${age} years old.`;
+console.log("Some info about me:", info);
+
+const favNum1 = 12;
+const favNum2 = 23;
+let sum = favNum1 + favNum2;
+console.log("Sum of two numbers:", sum);
+
+const product = favNum1 * favNum2;
+console.log("Product of two numbers:", product);
